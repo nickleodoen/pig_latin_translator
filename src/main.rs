04f1265b370f67713acaaae5195c6a698077fd6b
nikhil <nikhilram@gmail.com>
2022-05-26 00:26:01 +0530

    removed redundant comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 // Nikhil Yachareni - 05/25/2022
 use std::io;
 
-// Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, 
-// so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes 
-// “apple-hay”). Keep in mind the details about UTF-8 encoding!
-
 fn main() {
     let mut english = String::new();
     let mut pig_latin = String::new();
